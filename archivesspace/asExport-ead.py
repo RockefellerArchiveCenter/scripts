3519b1270e41a9ad4e21fc2a2bd48741a6a95eaf
helrond <helrond@hotmail.com>
2015-08-10 12:50:10 -0400

    removing incremental scripts

--- archivesspace/asExport-ead.py
+++ archivesspace/asExport-ead.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, requests, json, sys
+import os, requests, json, sys, logging
 
 # the base URL of your ArchivesSpace installation
 baseURL = 'http://localhost:8089'
@@ -13,28 +13,45 @@ password = 'admin'
 # parses arguments, if any. This allows you to pass in an string to match against resource IDs
 exportIds = sys.argv[1]
 # export destination
-destination = '/Users/harnold/Desktop/ead/'
+destination = '/home/harnold/test/'
+logging.basicConfig(filename='simple-log.txt',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 # authenticates the session
-auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
-session = auth["session"]
-headers = {'X-ArchivesSpace-Session':session}
+def authenticate():
+    auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+    session = auth["session"]
+    headers = {'X-ArchivesSpace-Session':session}
+    return headers
+
+headers = authenticate()
 
 # Gets the IDs of all resources in the repository
-print 'Getting a list of resources'
+logging.info('Getting a list of resources')
 resourceIds = requests.get(baseURL + '/repositories/'+repository+'/resources?all_ids=true', headers=headers)
 
 # Exports EAD for all resources whose IDs contain argument
 for id in resourceIds.json():
+    if not requests.get(baseURL + '/repositories/'+repository+'/resources/' + str(id), headers=headers):
+        headers = authenticate()
     resource = (requests.get(baseURL + '/repositories/'+repository+'/resources/' + str(id), headers=headers)).json()
     resourceID = resource["id_0"]
-    if exportIds in resourceID:
-        print 'Exporting ' + resourceID
+    if exportIds:
+        if exportIds in resourceID:
+            logging.info('Exporting ' + resourceID)
+            ead = requests.get(baseURL + '/repositories/'+repository+'/resource_descriptions/'+str(id)+'.xml', headers=headers, stream=True)
+            if not os.path.exists(destination):
+                os.makedirs(destination)
+            with open(destination+resourceID+'.xml', 'wb') as f:
+                for chunk in ead.iter_content(10240):
+                    f.write(chunk)
+            f.close
+    else:
+        logging.info('Exporting ' + resourceID)
         ead = requests.get(baseURL + '/repositories/'+repository+'/resource_descriptions/'+str(id)+'.xml', headers=headers, stream=True)
         if not os.path.exists(destination):
             os.makedirs(destination)
         with open(destination+resourceID+'.xml', 'wb') as f:
-            for chunk in ead.iter_content(102400):
+            for chunk in ead.iter_content(10240):
                 f.write(chunk)
         f.close
-print 'Done!'
+logging.info('Done!')
