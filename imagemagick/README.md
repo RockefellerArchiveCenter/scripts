7781a34fd36b297ab90b30b5117b8f97c16ac0ec
helrond <helrond@hotmail.com>
2015-10-12 14:48:27 -0400

    move files after creating thumbnails, deleting extra script

--- imagemagick/README.md
+++ imagemagick/README.md
@@ -2,10 +2,7 @@
 These scripts create thumbnail derivatives of digital objects for use on DIMES, plus additional actions.
 
 ##makeThumb.sh
-Creates several different derivative thumbnails of digital objects.
-
-##moveFile.sh
-Moves files from tmp directory to web accessible directory.
+Creates several different derivative thumbnails of digital objects and then moves files from a temporary directory to a web accessible directory.
 
 ##makeThumbRepair.sh
 Does the same thing as makeThumbs.sh, but is intended to be run once objects have already been moved to web accessible directory.
--- imagemagick/makeThumb.sh
+++ imagemagick/makeThumb.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 TMP=/var/tmp
+WWW=/var/www
 cd $TMP;
 # Make thumbnails from first page of PDFs
 if [ "$(ls -A $TMP)" ]; then
-    for file in `ls *.pdf`; do
-    sudo chmod 777 $file
-    #Make a thumbnail with a height of 100px
-    convert $file[0] -thumbnail 'x100' `echo $file | sed 's/\.pdf$/\_thumb.jpg/'`
-    #Make a square thumbnail 75x75 px
-    convert $file[0] -thumbnail '75x75^' -gravity 'Center' -crop '75x75+0+0' `echo $file | sed 's/\.pdf$/\_thumb75.jpg/'`
-    #Make a square thumbnail 75x75 px
-    convert $file[0] -thumbnail '300x300^' -gravity 'Center' -crop '300x300+0+0' `echo $file | sed 's/\.pdf$/\_thumb300.jpg/'`
-    #Make a large file with proportions of 1.9w to 1h
-    convert $file[0] -gravity 'North' -crop '100%x53%+0+0' `echo $file | sed 's/\.pdf$/\_thumbfb.jpg/'`
-done
-
-else
-echo 'No files to convert'
+  for file in `ls *.pdf`; do
+  sudo chmod 777 $file
+  #Make a thumbnail with a height of 100px
+  convert $file[0] -thumbnail 'x100' `echo $file | sed 's/\.pdf$/\_thumb.jpg/'`
+  #Make a square thumbnail 75x75 px
+  convert $file[0] -thumbnail '75x75^' -gravity 'Center' -crop '75x75+0+0' `echo $file | sed 's/\.pdf$/\_thumb75.jpg/'`
+  #Make a square thumbnail 75x75 px
+  convert $file[0] -thumbnail '300x300^' -gravity 'Center' -crop '300x300+0+0' `echo $file | sed 's/\.pdf$/\_thumb300.jpg/'`
+  #Make a large file with proportions of 1.9w to 1h
+  convert $file[0] -gravity 'North' -crop '100%x53%+0+0' `echo $file | sed 's/\.pdf$/\_thumbfb.jpg/'`
+  done
+  else
+  echo 'No files to convert'
+fi
+if [ "$(ls -A $TMP)" ]; then
+  for file in `ls *.*`; do
+      sudo mv $file $WWW
+  done
+  echo 'All files moved'
+  cd $WWW
+  for file in `ls *.*`; do
+      sudo chmod 664 $file
+  done
+  else
+  echo 'No files to move'
 fi
