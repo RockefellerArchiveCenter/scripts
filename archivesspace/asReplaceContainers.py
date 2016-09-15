025bb78a00f83bd6bc50640e73286e7a4494f603
helrond <helrond@hotmail.com>
2016-09-15 10:54:34 -0400

    fixing post url and adding argument option

--- archivesspace/asReplaceContainers.py
+++ archivesspace/asReplaceContainers.py
@@ -9,8 +9,6 @@ config.read('local_settings.cfg')
 logging.basicConfig(filename=config.get('Logging', 'filename'),format=config.get('Logging', 'format', 1), datefmt=config.get('Logging', 'datefmt', 1), level=config.get('Logging', 'level', 0))
 # Sets logging of requests to WARNING to avoid unneccessary info
 logging.getLogger("requests").setLevel(logging.WARNING)
-# Adds randomly generated commit message from external text file
-#commitMessage = line = random.choice(open(config.get('Git', 'commitMessageData')).readlines());
 
 dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':config.get('ArchivesSpace', 'repository'), 'user': config.get('ArchivesSpace', 'user'), 'password': config.get('ArchivesSpace', 'password')}
 repositoryBaseURL = '{baseURL}/repositories/{repository}'.format(**dictionary)
@@ -25,8 +23,7 @@ resource_containers = []
 
 # authenticates the session
 auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
-session = auth['session']
-headers = {'X-ArchivesSpace-Session':session}
+headers = {'X-ArchivesSpace-Session':auth['session']}
 
 def promptForIdentifier():
 	identifier = raw_input("Please enter a resource identifier: ")
@@ -48,26 +45,32 @@ def getRefs(data, resource_containers):
 			getRefs(component["children"], resource_containers)
 	return resource_containers
 
-def checkArchivalObject(archival_object, aoId, headers):
+def checkArchivalObject(archival_object, aoUri, headers):
 	if len(archival_object['instances']) > 0:
 		for instance in archival_object['instances']:
 			for index, container_uri in enumerate(replace_containers):
 				if ('sub_container' in instance):
 					if (instance['sub_container']['top_container']['ref'] == container_uri):
 						instance['sub_container']['top_container']['ref'] = keep_containers[index]
-						post = requests.post(repositoryBaseURL + '/archival_objects/'+str(aoId), headers=headers, data=json.dumps(archival_object))
-						print 'Container '+replace_containers[index]+' was replaced with container '+keep_containers[index]+' in archival object '+str(aoId)
-						logging.info('Container '+replace_containers[index]+' was replaced with container '+keep_containers[index]+' in archival object '+str(aoId))
+						post = requests.post('{baseURL}'.format(**dictionary) + str(aoUri), headers=headers, data=json.dumps(archival_object))
+						if(post.status_code == requests.codes.ok):
+							print 'Container '+replace_containers[index]+' was replaced with container '+keep_containers[index]+' in archival object '+str(aoUri)
+							logging.info('Container '+replace_containers[index]+' was replaced with container '+keep_containers[index]+' in archival object '+str(aoUri))
+						else:
+							print post.text
+							logging.error(post.text)
 	else:
 		pass
 
-identifier = promptForIdentifier()
+if not len(sys.argv) > 1:
+	identifier = promptForIdentifier()
+else:
+	identifier = sys.argv[1]
 print 'Getting a list of archival objects'
-aoIds = getResourceObjects(identifier, headers, resource_containers)
-print aoIds
+aoUris = getResourceObjects(identifier, headers, resource_containers)
 logging.info('Find and replace operation started')
-for aoId in aoIds:
-	ao = (requests.get('{baseURL}'.format(**dictionary) + str(aoId), headers=headers)).json()
-	print 'Checking archival object ' + str(aoId)
-	checkArchivalObject(ao, aoId, headers)
+for aoUri in aoUris:
+	ao = (requests.get('{baseURL}'.format(**dictionary) + str(aoUri), headers=headers)).json()
+	print 'Checking archival object ' + str(aoUri)
+	checkArchivalObject(ao, aoUri, headers)
 logging.info('Find and replace operation ended')
