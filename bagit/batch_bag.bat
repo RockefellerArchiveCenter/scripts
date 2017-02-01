5ec5ad96c1a9c1551a44f3e8ff9e12d031285550
pjg250 <patjames@umich.edu>
2017-02-01 10:13:55 -0500

    Create batch_bag.bat
    Use to batch create many bags at once. Just change filenames/locations.

---
+++ bagit/batch_bag.bat
@@ -0,0 +1,3 @@
+c:\
+cd c:\bagit-4.4\bin
+bag create Y:\HudsonMicroImaging\RF_Diaries\Tennant_01_bag Y:\HudsonMicroImaging\RF_Diaries\Tennant_01_bag_old\data\* && bag create Y:\HudsonMicroImaging\RF_Diaries\Tennant_02_bag Y:\HudsonMicroImaging\RF_Diaries\Tennant_02_bag_old\data\* 
