27c04cd2b6fe59ca503d1671445b38f9ce625717
EcZX <echang@rockarch.org>
2017-07-31 09:47:04 -0400

    Update Instance.py

--- archivesspace/Instance.py
+++ archivesspace/Instance.py
@@ -11,36 +11,60 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 # Adds randomly generated commit message from external text file
 #commitMessage = line = random.choice(open(config.get('Git', 'commitMessageData')).readlines());
 
+dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':config.get('ArchivesSpace', 'repository'), 'user': config.get('ArchivesSpace', 'user'), 'password': config.get('ArchivesSpace', 'password')}
+repositoryBaseURL = '{baseURL}/repositories/{repository}'.format(**dictionary)
+resourceURL = '{baseURL}'.format(**dictionary)
+
+column_names = ['keep_uri', 'replace_uri']
+data = pandas.read_csv('containers.csv', names=column_names)
+
+replace_containers = data.replace_uri.tolist()
+keep_containers = data.keep_uri.tolist()
+resource_containers = []
+
+
+
+
+# authenticates the session
+auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
+session = auth['session']
+headers = {'X-ArchivesSpace-Session':session}
 
 
 
 
 
-
-
-
+def promptForIdentifier():
+	identifier = raw_input("Please enter an ArchivesSpace resource identifier: ")
+	if identifier:
+		return identifier
+	else:
+		print "You didn't enter anything!"
+		promptForIdentifier()
 
 
 # get a list of identifers
-def getArchivalobjectsID ()
-
+def getArchivalobjectsID (identifier, archival_objects, instances)
+		tree = requests.get(respositoryBaseURL + "/resources" + identifier + "/instances", headers=headers).json()
+		AOIDS = ao(tree["instances"], archival_objects)
+		return AOIDS
 
 
 #This wil get get the data from the archival object URL
-def getAOData (AOIDs)
-
-
-
 def checkInstanceType (Moving Images, Audio)
+	if 
 
 
-def MakeList ResourceList
+
+def MakeList ResourceList (headers)
+	instances = ao["instances"]
+	for index, n in enumerate(instances):
+
+
+
+
 
 
 def deduplicates_list (Resources URI)
+	instances = ao["instances"]
 
-
-
-
-
-def getResourceData 
