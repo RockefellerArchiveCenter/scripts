712b7add8322ad54ad281377560ad3cade4d7487
p-galligan <pjg250@gmail.com>
2017-08-22 16:43:16 -0400

    cleanup and update
    Massive cleanup and update so that the script doesn't have to run through all archival objects each time it runs. Remove some redundant code for box and folders. Change authentication.

--- archivesspace/advancedNoteEdit.py
+++ archivesspace/advancedNoteEdit.py
@@ -16,58 +16,32 @@ dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':co
 repositoryBaseURL = '{baseURL}/repositories/{repository}'.format(**dictionary)
 resourceURL = '{baseURL}'.format(**dictionary)
 
+resource_containers = []
+
 # authenticates the session
 auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
 session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session}
 
-def findResourceId (headers):
-# Gets the resource id_0 for each archival object
-    try:
-        uri = ao["resource"].get('ref')
-        resource = (requests.get(resourceURL + str(uri), headers=headers)).json()
-        global resourceID
-        resourceID = resource["id_0"]
-        #print resourceID
-    except:
-        logging.warning('Archival Object ' + str(aoId) + ' is not connected to a resource and will be skipped')
-        resourceID = 'Archival Object is not associated with a resource and will be skipped'
+def promptForIdentifier():
+	identifier = raw_input("Please enter a resource identifier: ")
+	if identifier:
+		return identifier
+	else:
+		print "You didn't enter anything!"
+		promptForIdentifier()
 
-def findBoxFolder(headers):
-# Gets the box and folder number of each instance
-    instances = ao["instances"]
-    for index, n in enumerate(instances):
-        global ctypeone
-        global cindicatorone
-        global ctypetwo
-        global cindicatortwo
-        global boxfolder
-        ctypeone = 0
-        cindicatorone = 0
-        ctypetwo = 0
-        cindicatortwo = 0
-        try:
-            ctypeone = n["container"]["type_1"]
-        except:
-            pass
-        try:
-            cindicatorone = n["container"]["indicator_1"]
-        except:
-            pass
-        try:
-            ctypetwo = n["container"]["type_2"]
-        except:
-            pass
-        try:
-            cindicatortwo = n["container"]["indicator_2"]
-        except:
-            pass
-        if (ctypeone == 0 and cindicatorone == 0):
-            boxfolder = 'But no containers found'
-        elif (ctypeone != 0 and cindicatorone != 0 and ctypetwo == 0 and cindicatortwo == 0):
-            boxfolder = u' '.join((ctypeone, cindicatorone)).encode('utf-8').strip()
-        elif (ctypeone != 0 and cindicatorone != 0 and ctypetwo != 0 and cindicatortwo != 0):
-            boxfolder = u' '.join((ctypeone, cindicatorone, ctypetwo, cindicatortwo)).encode('utf-8').strip()
+def getResourceObjects(identifier, headers, resource_containers):
+	tree = requests.get(repositoryBaseURL + "/resources/" + identifier + "/tree", headers=headers).json()
+	refList = getRefs(tree["children"], resource_containers)
+	return refList
+
+def getRefs(data, resource_containers):
+	for component in data:
+		resource_containers.append(component["record_uri"])
+		if component["has_children"]:
+			getRefs(component["children"], resource_containers)
+	return resource_containers
 
 def deleteNotes(headers):
 # Deletes AccessRestrict notes that match input notecontent
@@ -76,14 +50,10 @@ def deleteNotes(headers):
         try:
             if n["type"] == notetype:
                 for subnote in n["subnotes"]:
-                    if notecontent == subnote["content"] and boxfolder != []:
+                    if notecontent == subnote["content"]:
                         del notes[index]
-                        updated = requests.post(repositoryBaseURL + '/archival_objects/' + str(aoId), headers=headers, data=json.dumps(ao))
-                        logging.info('Deleted access restrict note with ' + str(notecontent) + ' content from archival object ' + str(aoId) + ' in resource ' + str(resourceID) + ' from ' + str(boxfolder))
-                    elif notecontent == subnote["content"] and boxfolder == []:
-                        del notes[index]
-                        updated = requests.post(repositoryBaseURL + '/archival_objects/' + str(aoId), headers=headers, data=json.dumps(ao))
-                        logging.info('Deleted access restrict note with ' + str(notecontent) + ' content from archival object ' + str(aoId) + ' in resource ' + str(resourceID) + ' with no instances')
+                        post = requests.post('{baseURL}'.format(**dictionary) + str(aoId), headers=headers, data=json.dumps(ao))
+                        logging.info('Deleted note with ' + str(notecontent) + ' content from archival object ' + str(aoId) + ' in resource ' + str(resourceID))
         except:
             pass
 
@@ -94,19 +64,15 @@ def replaceNotes(headers):
         try:
             if n["type"] == notetype:
                 for subnote in n["subnotes"]:
-                    if notecontent == subnote["content"] and boxfolder != []:
+                    if notecontent == subnote["content"]:
                         subnote["content"] = replacecontent
-                        updated = requests.post(repositoryBaseURL + '/archival_objects/' + str(aoId), headers=headers, data=json.dumps(ao))
-                        logging.info('Replacing note with ' + str(notecontent) + ' content with ' + str(replacecontent) + ' content in archival object ' + str(aoId) + ' in resource ' + str(resourceID) + ' from ' + str(boxfolder))
-                    elif notecontent == subnote["content"] and boxfolder == []:
-                        subnote["content"] = replacecontent
-                        updated = requests.post(repositoryBaseURL + '/archival_objects/' + str(aoId), headers=headers, data=json.dumps(ao))
-                        logging.info('Replacing note with ' + str(notecontent) + ' content with ' + str(replacecontent) + ' content in archival object ' + str(aoId) + ' in resource ' + str(resourceID) + ' with no instances')
+                        post = requests.post('{baseURL}'.format(**dictionary) + str(aoId), headers=headers, data=json.dumps(ao))
+                        logging.info('Replacing note with ' + str(notecontent) + ' content with ' + str(replacecontent) + ' content in archival object ' + str(aoId) + ' in resource ' + str(resourceID))
         except:
             pass
 
 #asks user to input note content
-resourcesearch = raw_input('Enter resorce id number: ')
+identifier = promptForIdentifier()
 objectlevel = raw_input('Enter archival object level: ')
 notetype = raw_input('Enter the type of note (use EAD note types, e.g. accessrestrict): ')
 notecontent = raw_input('Enter note content: ')
@@ -114,14 +80,12 @@ modifydelete = raw_input('Modify notes or delete notes? (modify/delete) ')
 if modifydelete == 'modify':
     replacecontent = raw_input('New note content: ')
 print 'Getting a list of archival objects'
-aoIds = requests.get(repositoryBaseURL + '/archival_objects?all_ids=true', headers=headers)
-for aoId in aoIds.json():
-    ao = (requests.get(repositoryBaseURL + '/archival_objects/' + str(aoId), headers=headers)).json()
+aoIds = getResourceObjects(identifier, headers, resource_containers)
+for aoId in aoIds:
+    ao = (requests.get('{baseURL}'.format(**dictionary) + str(aoId), headers=headers)).json()
     boxfolder = []
-    findResourceId(headers)
-    findBoxFolder(headers)
     levels = ao["level"]
-    if  modifydelete == 'delete' and resourceID != 'Archival Object is not associated with a resource and will be skipped' and resourceID == resourcesearch and levels == objectlevel:
+    if  modifydelete == 'delete' and levels == objectlevel:
         deleteNotes(headers)
-    elif modifydelete == 'modify' and resourceID != 'Archival Object is not associated with a resource and will be skipped' and resourceID == resourcesearch and levels == objectlevel:
+    elif modifydelete == 'modify' and levels == objectlevel:
         replaceNotes(headers)
