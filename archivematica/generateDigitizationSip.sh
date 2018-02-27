c4fdb485789c176159fff447772156ce5c57bb1f
bonniegee <bgordon@rockarch.org>
2018-02-27 17:03:48 -0500

    create directory and subdirectories

--- archivematica/generateDigitizationSip.sh
+++ archivematica/generateDigitizationSip.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 # input refid
+read -p "Enter the ArchivesSpace refid: " refid
 
 #  create directory and subdirectories
-
+mkdir ~/archivematica_test/archivematica_sip_$refid
+cd ~/archivematica_test/archivematica_sip_$refid
+mkdir logs
+mkdir metadata
+mkdir objects
+mkdir objects/access
+mkdir objects/tiffs
 
 # copy service edited files to /access folder (and objects folder if concatenated file)
 
