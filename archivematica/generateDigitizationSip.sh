6b70410a51a806962f00252d7e92db0b00c741b3
bonniegee <bgordon@rockarch.org>
2018-02-27 16:38:50 -0500

    add comments outlining steps

--- archivematica/generateDigitizationSip.sh
+++ archivematica/generateDigitizationSip.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+# input refid
+
+#  create directory and subdirectories
+
+
+# copy service edited files to /access folder (and objects folder if concatenated file)
+
+
+# copy master edited files to objects folder
+
+
+# copy master files to /tiffs folder
