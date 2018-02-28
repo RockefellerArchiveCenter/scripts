8ea9a6b8ddb5a0324c4aa484e24b833142498837
bonniegee <bgordon@rockarch.org>
2018-02-28 14:26:36 -0500

    add filename cleanup

--- archivematica/generateDigitizationSip.sh
+++ archivematica/generateDigitizationSip.sh
@@ -12,24 +12,48 @@ mkdir objects
 mkdir objects/access
 mkdir objects/service
 
-# find master files
+# find master files and copy master files to objects folder
+cd ~/archivematica_test/Master
+array=(`find ${refid}*`)
+for i in ${array[@]}
+do
+	echo $i
+	cp $i ~/archivematica_test/archivematica_sip_$refid/objects
+done
 
-# copy master files to objects folder
 
-
-# find service edited files
-
-
-# copy service edited files to /access folder (and objects folder if concatenated file)
+# find service edited files and copy service edited files to /access folder (and objects folder if concatenated file)
+cd ~/archivematica_test/Service\ Edited
+array=(`find ${refid}*`)
+for i in ${array[@]}
+do
+	echo $i
+	cp $i ~/archivematica_test/archivematica_sip_$refid/objects/access
+done
 
 
 # remove "_se" from files in /access folder
+cd ~/archivematica_test/archivematica_sip_$refid/objects/access
+array=(`ls`)
+for i in ${array[@]}
+do
+	mv $i "${i/_se/}"
+done
 
 
-# find master edited files
-
-
-# copy master edited files to /service folder
-
+# find master edited files and copy master edited files to /service folder
+cd ~/archivematica_test/Master\ Edited
+array=(`find ${refid}*`)
+for i in ${array[@]}
+do
+	echo $i
+	cp $i ~/archivematica_test/archivematica_sip_$refid/objects/service
+done
 
 # remove "_me" from files in /access folder
+cd ~/archivematica_test/archivematica_sip_$refid/objects/service
+array=(`ls`)
+for i in ${array[@]}
+do
+	mv $i "${i/_me/}"
+done
