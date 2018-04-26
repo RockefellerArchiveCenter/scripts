150afc661ae817a0bb6ff2ff7986bcec953212e6
Hillel Arnold <helrond@hotmail.com>
2018-04-26 14:43:59 -0400

    debugging, adding start and end date functionality

--- archivesspace/asCSV-accessions.py
+++ archivesspace/asCSV-accessions.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
-import os, requests, json, logging, ConfigParser, csv
-from codecs import encode
-from codecs import decode
+import os
+import requests
+import json
+import logging
+import ConfigParser
+import csv
+import time
+from datetime import datetime
+from codecs import encode, decode
 
 config = ConfigParser.ConfigParser()
 config.read('local_settings.cfg')
 
 # Logging configuration
-logging.basicConfig(filename=config.get('Logging', 'filename'),format=config.get('Logging', 'format', 1), datefmt=config.get('Logging', 'datefmt', 1), level=config.get('Logging', 'level', 0))
+logging.basicConfig(
+    filename=config.get('Logging', 'filename'),
+    format=config.get('Logging', 'format', 1),
+    datefmt=config.get('Logging', 'datefmt', 1),
+    level=config.get('Logging', 'level', 0))
+
 # Sets logging of requests to WARNING to avoid unneccessary info
 logging.getLogger("requests").setLevel(logging.WARNING)
 
-dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':config.get('ArchivesSpace', 'repository'), 'user': config.get('ArchivesSpace', 'user'), 'password': config.get('ArchivesSpace', 'password')}
+dictionary = {
+    'baseURL': config.get('ArchivesSpace', 'baseURL'),
+    'repository': config.get('ArchivesSpace', 'repository'),
+    'user': config.get('ArchivesSpace', 'user'),
+    'password': config.get('ArchivesSpace', 'password')}
+
 repositoryBaseURL = '{baseURL}/repositories/{repository}'.format(**dictionary)
 resourceURL = '{baseURL}'.format(**dictionary)
 
@@ -27,13 +43,14 @@ writer = csv.writer(open('accessions.csv', 'w'))
 
 def get_extent_data(object):
     data = {}
-    for extent in object['extents']:
-        if extent['portion'] == 'whole':
-            container_summary = extent['container_summary'] if 'container_summary' in extent else ''
-            data = dict(
-                [('container_summary', container_summary),
-                 ('extent_number', extent['number']),
-                 ('extent_type', extent['extent_type'])])
+    if 'extents' in object:
+        for extent in object['extents']:
+            if extent['portion'] == 'whole':
+                container_summary = extent.get('container_summary', None)
+                data = dict(
+                    [('container_summary', container_summary),
+                     ('extent_number', extent['number']),
+                     ('extent_type', extent['extent_type'])])
     return data
 
 
@@ -48,9 +65,9 @@ def get_delivery_date(object):
 def get_number(object):
     number = object['id_0']
     if 'id_1' in object:
-        number = number + '.' + object['id_1']
+        number = '{}.{}'.format(number, object['id_1'])
         if 'id_2' in object:
-            number = number + '.' + object['id_2']
+            number = '{}.{}'.format(number, object['id_2'])
     return number
 
 
@@ -75,13 +92,33 @@ def make_row(accession):
 
 
 def main():
+    created_before = datetime.now()
+    created_after = start_date_entry = datetime(1974, 01, 01, 0, 0)
+    start_date_entry = raw_input('Enter a start date in YYYY-MM-DD format (hit enter for no start date): ')
+    end_date_entry = raw_input('Enter an end date in YYYY-MM-DD format (hit enter to export everything after the start date): ')
+    if len(end_date_entry):
+        created_before = datetime.strptime(str(end_date_entry), "%Y-%m-%d")
+    if len(start_date_entry):
+        created_after = datetime.strptime(str(start_date_entry), "%Y-%m-%d")
+
     column_headings = ['accession date', 'title', 'number', 'container summary',
                        'extent number', 'extent type', 'acquisition type', 'delivery date']
     writer.writerow(column_headings)
-    accessionIds = requests.get(repositoryBaseURL + '/accessions?all_ids=true', headers=headers).json()
+
+    accessionIds = requests.get(
+        '{}/accessions'.format(repositoryBaseURL), headers=headers,
+        params={"all_ids": True}).json()
+
+    if 'error' in accessionIds:
+        print accessionIds['error']
+        return False
+
     for accessionId in reversed(accessionIds):
-        accession = (requests.get(repositoryBaseURL + '/accessions/' + str(accessionId), headers=headers)).json()
-        print 'Writing accession', str(accessionId)
-        make_row(accession)
+        accession = (requests.get('{}/accessions/{}'.format(repositoryBaseURL, str(accessionId)), headers=headers)).json()
+        accession_created = datetime.strptime(accession['create_time'], '%Y-%m-%dT%H:%M:%SZ')
+        if (accession_created > created_after) and (accession_created < created_before):
+            print 'Writing accession', str(accessionId)
+            make_row(accession)
+
 
 main()
