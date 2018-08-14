bea8b349fef2025e1d237e62c68a47be30f06bda
bonniegee <bgordon@rockarch.org>
2018-08-14 14:01:32 -0400

    update copyright row function

--- archivematica/premisCsvDigitization.py
+++ archivematica/premisCsvDigitization.py
@@ -9,7 +9,7 @@ metadata_directory = 'archivematica_sip_' + refid + '/metadata/'
 # filenames = os.listdir(objects_directory)
 # print filenames
 
-# GET COPYRIGHT INFO
+# GET COPYRIGHT BASIS INFO
 
 # copyright status
 def getCopyrightStatus():
@@ -47,8 +47,24 @@ def getCopyrightEndDate():
     copyright_end_date = str(copyright_end_year) + "-01-01"
     return copyright_end_date
 
-# GET DONOR INFO
+# get permission/restriction on the act
+def getRestriction():
+    restriction_input = raw_input("allow, conditional, or disallow (a,c,d)")
+    if restriction_input.lower() == 'a' or restriction_input.lower() == 'allow':
+        restriction = 'allow'
+    elif restriction_input.lower() == 'c' or restriction_input.lower() == 'conditional':
+        restriction = 'conditional'
+    elif restriction_input.lower() == 'd' or restriction_input.lower() == 'disallow':
+        restriction = 'disallow'
+    else:
+        print("invalid")
+        getCopyrightRestriction()
+    return restriction
 
+# GET DONOR BASIS INFO
+
+
+# ACT INFO
 
 # find and store all objects
 
@@ -77,18 +93,20 @@ def makeRow(filename,basis,status,determination_date,jurisdiction,start_date,end
     writer.writerow(row)
 
 def makeCopyrightRow(filename):
+    filename = "objects/" + filename
     basis = 'copyright'
     status = getCopyrightStatus()
     determination_date = getCopyrightDeterminationDate()
     jurisdiction = 'us'
-    start_date = '???'
-    end_date =  '???'
-    note = '???'
+    start_date = getCopyrightStartDate()
+    end_date =  getCopyrightEndDate()
+    note = 'This Item is protected by copyright and/or related rights. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use. For other uses you need to obtain permission from the rights-holder(s).'
     grant_act = 'publish'
-    grant_restriction = 'allow'
-    grant_start_date = '2016-01-04'
+    print(grant_act)
+    grant_restriction = getRestriction()
+    grant_start_date = '2018-06-01'
     grant_end_date = 'open'
-    grant_note = '???'
+    grant_note = ''
     makeRow(filename,basis,status,determination_date,jurisdiction,start_date,end_date,note,grant_act,grant_restriction,grant_start_date,grant_end_date,grant_note)
 
 
