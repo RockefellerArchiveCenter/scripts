1bdea10db0742643ddef7b5834138988e28cff5f
bonniegee <bgordon@rockarch.org>
2019-04-23 16:09:33 -0400

    comment out master_edited lines

--- archivematica/generateDigitizationSip.sh
+++ archivematica/generateDigitizationSip.sh
@@ -12,7 +12,7 @@ mkdir ${topDirectory}/archivematica_sip_${refid}
 targetDirectory="${topDirectory}/archivematica_sip_${refid}"
 mkdir ${targetDirectory}/logs ${targetDirectory}/metadata
 mkdir -p ${targetDirectory}/objects/access
-mkdir ${targetDirectory}/objects/service
+# mkdir ${targetDirectory}/objects/service
 
 # find master files and copy master files to objects folder
 array=(`ls ${topDirectory}/byRefId/${refid}/master`)
@@ -46,22 +46,21 @@ if [[ " ${array[*]} " == *".pdf"* ]]; then
 fi
 
 # find master edited files and copy master edited files to /service folder
-array=(`ls ${topDirectory}/byRefId/${refid}/master_edited`)
-for i in ${array[@]}
-do
-	echo $i
-	cp ${topDirectory}/byRefId/${refid}/master_edited/$i ${targetDirectory}/objects/service/${i/_me/}
-	# check number of files in service directory equal number of files in objects directory
-	
-done
+# array=(`ls ${topDirectory}/byRefId/${refid}/master_edited`)
+# for i in ${array[@]}
+# do
+# 	echo $i
+# 	cp ${topDirectory}/byRefId/${refid}/master_edited/$i ${targetDirectory}/objects/service/${i/_me/}
+# 	# check number of files in service directory equal number of files in objects directory
+# done
 
 rm ${targetDirectory}/objects/Thumbs.db
 master_filelist_count=(`ls ${targetDirectory}/objects/ | wc -l`)
 echo "There are ${master_filelist_count} files and directories in the /objects directory"
 
-rm ${targetDirectory}/objects/service/Thumbs.db
-service_filelist_count=(`ls ${targetDirectory}/objects/service/ | wc -l`)
-echo "There are ${service_filelist_count} files and directories in the /objects/service directory"
+# rm ${targetDirectory}/objects/service/Thumbs.db
+# service_filelist_count=(`ls ${targetDirectory}/objects/service/ | wc -l`)
+# echo "There are ${service_filelist_count} files and directories in the /objects/service directory"
 
 rm ${targetDirectory}/objects/access/Thumbs.db
 access_filelist_count=(`ls ${targetDirectory}/objects/access/ | wc -l`)
