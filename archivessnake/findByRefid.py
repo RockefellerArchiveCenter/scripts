d421da8433becfc589d5369183ae1c4932a85c17
bonniegee <bgordon@rockarch.org>
2019-08-28 14:55:35 -0400

    combine note functions

--- archivessnake/findByRefid.py
+++ archivessnake/findByRefid.py
@@ -93,19 +93,11 @@ def getAoLevel(ao):
     # get level of description
     return ao.get("level")
 
-def getAccessRestriction(ao):
-    # iterate through archival object's notes, check for conditions governing access, return note
+def getSpecificNote(ao, noteType):
     if ao.get("notes"):
         for n in ao.get("notes"):
-            if n.get("type") == "accessrestrict":
-                return n.get("subnotes")[0].get("content").replace('\n', ' ') # replace line break with space
-
-def getUseRestriction(ao):
-    # iterate through archival object's notes, check for conditions governing use, return note
-    if ao.get("notes"):
-        for n in ao.get("notes"):
-            if n.get("type") == "userestrict":
-                return n.get("subnotes")[0].get("content").replace('\n', ' ') # replace line break with space
+            if n.get("type") == noteType: #accessrestrict, userestrict
+                return n.get("subnotes")[0].get("content").strip().replace('\n', ' ') # replace line break with space
 
 def getAoNotes(ao):
     # get note type for each note, append to list
@@ -120,26 +112,22 @@ def getAoNotes(ao):
         return ""
 
 def getAncestor(ao):
-#    print("number of ancestors: " + str(len(ao["ancestors"])))
-    # if archival object has a title, iterate through ancestors, return first ancestor that is not a resource
     if ao.get("title"):
-        for a in ao.get("ancestors"):
-            ancestor_url = a["ref"]
-            ancestor = client.get(ancestor_url).json()
-            if ancestor.get("jsonmodel_type") == "archival_object":
+        if ao.get("ancestors"):
+            if len(ao.get("ancestors")) >= 2:
+                x = len(ao.get("ancestors")) - 2
+                ancestor_url = ao.get("ancestors")[x].get("ref")
+                ancestor = client.get(ancestor_url).json()
                 return(ancestor)
     else:
-        # if archival object does not have a title, start one ancestor up, then iterate through ancestors, return first ancestor that is not a resource
         ancestor_url = ao.get("ancestors")[0].get("ref")
         ao = client.get(ancestor_url).json()
         if ao.get("ancestors"):
-            for a in ao.get("ancestors"):
-                ancestor_url = a.get("ref")
+            if len(ao.get("ancestors")) >= 2:
+                x = len(ao.get("ancestors")) - 2
+                ancestor_url = ao.get("ancestors")[x].get("ref")
                 ancestor = client.get(ancestor_url).json()
-                if ancestor.get("jsonmodel_type") == "archival_object":
-                    return(ancestor)
-        else:
-            ao.get("display_title")
+                return(ancestor)
 
 def getResource(ao):
     return client.get(ao["resource"]["ref"]).json()
@@ -147,26 +135,29 @@ def getResource(ao):
 def makeRow(ao,refid):
     row = []
     row.append(refid)
-    row.append(getTitle(ao).replace('\n', ' '))
-    row.append(getAoDates(ao))
-    row.append(getAccessRestriction(ao))
-    row.append(getAoNotes(ao))
-    if getAncestor(ao):
-        ancestor = getAncestor(ao)
-        row.append(getTitle(ancestor).replace('\n', ' ') + " (" + getAoLevel(ancestor) + ")")
-        row.append(getAccessRestriction(ancestor))
-        row.append(getAoNotes(ancestor))
+    row.append(getTitle(ao).strip())
+    if getAoDates(ao):
+        row.append(getAoDates(ao))
+    elif getAoDates(getResource(ao)):
+        if int(getAoDates(getResource(ao))) <= 1960:
+            row.append(getAoDates(getResource(ao)))
+        else:
+            row.append("")
     else:
         row.append("")
-        row.append("")
+    if getAncestor(ao):
+        ancestor = getAncestor(ao)
+        row.append(getTitle(ancestor).strip() + " (" + getAoLevel(ancestor) + ")")
+    else:
         row.append("")
     resource = getResource(ao)
-    row.append(resource["title"].split(', ')[0])
-    row.append(resource["title"].strip(resource["title"].split(', ')[0] + ", "))
+    resourceTitle = resource["title"]
+    parentCollection = resourceTitle.split(', ')[0]
+    row.append(parentCollection)
+    restOfFA = resourceTitle[(len(parentCollection) + 2):]
+    row.append(restOfFA.strip())
     row.append(resource["id_0"])
     row.append(getAoDates(resource))
-    row.append(getAccessRestriction(resource))
-    row.append(getUseRestriction(resource))
     writer.writerow(row)
 
 def makeSpreadsheet(filelist):
@@ -195,7 +186,7 @@ client.authorize()
 # create spreadsheet
 spreadsheet = open("findOnDemand.csv", "w")
 writer = csv.writer(spreadsheet)
-columnHeadings = ["RefId", "Title", "Component Dates", "Component Access Restricton", "Component Notes", "Ancestor", "Ancestor Access Restriction", "Ancestor Notes", "Parent Collection", "Finding Aid Title", "Resource ID", "Resource Dates", "Resource Access Restriction", "Resource Use Restriction"]
+columnHeadings = ["RefId", "Title", "Component Dates", "Ancestor", "Parent Collection", "Remainder of Finding Aid Title", "Resource ID", "Resource End Date"]
 writer.writerow(columnHeadings)
 
 fileList = open("refids.txt").readlines()
