57b5a34fb5942fc97c7411a87bf6f4ad81ed7305
Hillel Arnold <helrond@hotmail.com>
2020-01-10 11:34:19 -0500

    formatting

--- archivessnake/get_labels.py
+++ archivessnake/get_labels.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
-import argparse, configparser, json, csv
+import argparse
+import configparser
+import json
+import csv
 import os.path
 
 from asnake.aspace import ASpace
 
+
 class LabelPrinter:
     def __init__(self, resource):
         config = configparser.ConfigParser()
-        config.read('local_settings.cfg')
-        self.aspace = ASpace(baseurl=config.get('ArchivesSpace', 'baseURL'), username=config.get('ArchivesSpace', 'user'), password=config.get('ArchivesSpace', 'password'))
+        config.read("local_settings.cfg")
+        self.aspace = ASpace(
+            baseurl=config.get("ArchivesSpace", "baseURL"),
+            username=config.get("ArchivesSpace", "user"),
+            password=config.get("ArchivesSpace", "password"),
+        )
         self.resource = self.aspace.repositories(2).resources(resource)
 
     def run(self):
@@ -25,9 +33,9 @@ class LabelPrinter:
         self.make_csv(set(tuple(row) for row in label_data))
 
     def make_csv(self, label_data):
-        with open('box_labels.csv', 'w') as csvfile:
+        with open("box_labels.csv", "w") as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['ResourceTitle', 'ResourceID', 'ParentTitle', 'Container'])
+            writer.writerow(["ResourceTitle", "ResourceID", "ParentTitle", "Container"])
             writer.writerows(label_data)
 
     def get_id(self):
@@ -35,7 +43,7 @@ class LabelPrinter:
         Gets and returns the id of a resource record.
         """
         number = []
-        for n in range(0,3):
+        for n in range(0, 3):
             try:
                 number.append(getattr(self.resource, "id_{}".format(n)))
             except AttributeError:
@@ -50,7 +58,11 @@ class LabelPrinter:
         for instance in obj.instances:
             try:
                 top_container = instance.sub_container.top_container
-                containers.append("{} {}".format(top_container.type.capitalize(), top_container.indicator))
+                containers.append(
+                    "{} {}".format(
+                        top_container.type.capitalize(), top_container.indicator
+                    )
+                )
             except KeyError as e:
                 pass
         return ", ".join(containers)
@@ -65,7 +77,9 @@ class LabelPrinter:
             return ""
 
 
-parser = argparse.ArgumentParser(description="Creates a csv with container labels based on a a given resource ID.")
+parser = argparse.ArgumentParser(
+    description="Creates a csv with container labels based on a a given resource ID."
+)
 parser.add_argument("resource", help="ArchivesSpace resource id")
 args = parser.parse_args()
 
