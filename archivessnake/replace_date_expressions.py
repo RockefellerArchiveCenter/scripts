fd8532785590968c5b626f1ab40801e0ce558150
Hillel Arnold <helrond@hotmail.com>
2020-02-26 09:40:14 -0500

    update argparse description

--- archivessnake/replace_date_expressions.py
+++ archivessnake/replace_date_expressions.py
@@ -87,7 +87,7 @@ class ExpressionReplacer:
             print(updated.json())
 
 
-parser = argparse.ArgumentParser(description="Adds dates to components using ArchivesSpace's date calculator endpoint.")
+parser = argparse.ArgumentParser(description="Replaces problematic strings in date expressions and optionally updates the last saved time of all archival objects in a resource tree.")
 parser.add_argument('resource', help='ID of the resource whose tree should be processed.')
 parser.add_argument('-t', '--touch', action='store_true', help='Trigger a save on each archival object.')
 args = parser.parse_args()
