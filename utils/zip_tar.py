ba21ab4e0b7077dc96a845a8e5dddba77b92dc64
Hillel Arnold <helrond@hotmail.com>
2020-04-27 17:04:43 -0500

    refactors as oo approach

--- utils/zip_tar.py
+++ utils/zip_tar.py
@@ -6,36 +6,35 @@ import tarfile
 import zipfile
 
 
-def make_zip(base_path, dir):
-    output_filename = "{}.zip".format(os.path.basename(dir))
-    output_filepath = os.path.join(base_path, output_filename)
-    with zipfile.ZipFile(output_filepath, "w") as zip:
-        for folder, subfolders, filenames in os.walk(dir):
-            for f in filenames:
-                filepath = os.path.join(folder, f)
-                zip.write(filepath)
+class DirectorySerializer(object):
+    def __init__(self, source_dir):
+        self.base_path = source_dir
 
+    def run(self):
+        for f in os.listdir(self.base_path):
+            if os.path.isdir(os.path.join(self.base_path, f)):
+                self.make_tar(os.path.join(self.base_path, f))
+                self.make_targz(os.path.join(self.base_path, f))
+                self.make_zip(os.path.join(self.base_path, f))
 
-def make_tar(base_path, dir):
-    output_filename = "{}.tar".format(os.path.basename(dir))
-    output_filepath = os.path.join(base_path, output_filename)
-    with tarfile.open(output_filepath, "w") as tar:
-        tar.add(dir, arcname=os.path.basename(dir))
+    def get_filepath(self, dir, ext):
+        output_filename = "{}.{}".format(os.path.basename(dir), ext)
+        return os.path.join(self.base_path, output_filename)
 
+    def make_zip(self, dir):
+        with zipfile.ZipFile(self.get_filepath(dir, "zip"), "w") as zip:
+            for folder, subfolders, filenames in os.walk(dir):
+                for f in filenames:
+                    filepath = os.path.join(folder, f)
+                    zip.write(filepath)
 
-def make_targz(base_path, dir):
-    output_filename = "{}.tar.gz".format(os.path.basename(dir))
-    output_filepath = os.path.join(base_path, output_filename)
-    with tarfile.open(output_filepath, "w:gz") as tar:
-        tar.add(dir, arcname=os.path.basename(dir))
+    def make_tar(self, dir):
+        with tarfile.open(self.get_filepath(dir, "tar"), "w") as tar:
+            tar.add(dir, arcname=os.path.basename(dir))
 
-
-def main(source_dir):
-    for f in os.listdir(source_dir):
-        if os.path.isdir(os.path.join(source_dir, f)):
-            make_tar(source_dir, os.path.join(source_dir, f))
-            make_targz(source_dir, os.path.join(source_dir, f))
-            make_zip(source_dir, os.path.join(source_dir, f))
+    def make_targz(self, dir):
+        with tarfile.open(self.get_filepath(dir, "tar.gz"), "w:gz") as tar:
+            tar.add(dir, arcname=os.path.basename(dir))
 
 
 parser = argparse.ArgumentParser(description='Creates ZIP and TAR files from a list of directories.')
@@ -43,4 +42,4 @@ parser.add_argument('filepath', help='File path of JSON file to split into separ
 args = parser.parse_args()
 
 
-main(args.filepath)
+DirectorySerializer(args.filepath).run()
