700e6988ea13f0fb6c40139051a0d012d538963c
bonniegee <bgordon@rockarch.org>
2020-11-16 14:38:24 -0500

    remove csv code from main script

--- archivematica/microfilm-sips.py
+++ archivematica/microfilm-sips.py
@@ -2,8 +2,6 @@
 
 import argparse
 import os
-from csv import writer
-from datetime import datetime
 from shutil import copy2
 
 parser = argparse.ArgumentParser(description='Copies TIFF and PDF files.')
@@ -18,16 +16,6 @@ parser.add_argument(
     help='Directory name that corresponds to the microfilm collection.')
 parser.add_argument(
     'reel', help='Directory name that corresponds to the reel.')
-parser.add_argument(
-    '-a',
-    '--aspace',
-    help='Option to create the first column of an archivesspaceids.csv file.',
-    action='store_true')
-parser.add_argument(
-    '-p',
-    '--premis',
-    help='Option to a rights.csv file. Assumes creation year is in filename.',
-    action='store_true')
 args = parser.parse_args()
 
 # Create new folder and define name of new folder (probably use txt file
@@ -40,7 +28,12 @@ list_of_folders = [folder for folder in os.listdir(
 def package_transfer(sip_directory, reel_path, collection, reel, folder):
     print("Starting {} {} {}...".format(collection, reel, folder))
     make_sip_directory(sip_directory, collection, reel, folder)
-    sip_directory = os.path.join(args.sip_directory, "{}_{}_{}".format(collection.replace(" ", "_"), reel.replace(" ", "_"), folder.replace(" ", "_")))
+    sip_directory = os.path.join(
+        args.sip_directory, "{}_{}_{}".format(
+            collection.replace(
+                " ", "_"), reel.replace(
+                " ", "_"), folder.replace(
+                    " ", "_")))
     copy_files(
         os.path.join(
             reel_path, folder, "Master"), os.path.join(
@@ -67,7 +60,12 @@ def copy_files(source, destination):
 
 def make_sip_directory(topDirectory, collection, reel, folder):
     print("Making SIP directory...")
-    microfilmfolder = os.path.join(topDirectory, "{}_{}_{}".format(collection.replace(" ", "_"), reel.replace(" ", "_"), folder.replace(" ", "_")))
+    microfilmfolder = os.path.join(
+        topDirectory, "{}_{}_{}".format(
+            collection.replace(
+                " ", "_"), reel.replace(
+                " ", "_"), folder.replace(
+                    " ", "_")))
     os.mkdir(os.path.join(topDirectory, microfilmfolder))
     targetDirectory = os.path.join(topDirectory, microfilmfolder)
     for subdirectory in ["logs", "metadata", "objects"]:
@@ -76,132 +74,10 @@ def make_sip_directory(topDirectory, collection, reel, folder):
     os.mkdir(os.path.join(objectsDirectory, "access"))
 
 
-# Copy access files in folder
-
-
-def createAspaceCsv(metadata, access):
-    print("Creating archivesspaceids.csv...")
-    aspacecsv = os.path.join(metadata, 'archivesspaceids.csv')
-    with open(aspacecsv, 'w', newline='') as csvfile:
-        f = "objects/" + access
-        writer(csvfile).writerow([f])
-
-
-def getCopyrightEndDate(copyrightStartYear, copyrightTerm):
-    copyrightEndYear = int(copyrightStartYear) + int(copyrightTerm)
-    copyrightEndDate = str(copyrightEndYear) + "-12-31"
-    return copyrightEndDate
-
-
-def makeRow(
-        filename,
-        basis,
-        status,
-        determination_date,
-        jurisdiction,
-        start_date,
-        end_date,
-        note,
-        grant_act,
-        grant_restriction,
-        grant_start_date,
-        grant_end_date,
-        grant_note):
-    row = []
-    row.append(filename)
-    row.append(basis)
-    row.append(status)
-    row.append(determination_date)
-    row.append(jurisdiction)
-    row.append(start_date)
-    row.append(end_date)
-    # terms - for license basis #citation - for statute basis
-    row.extend([''] * 2)
-    row.append(note)
-    row.append(grant_act)
-    row.append(grant_restriction)
-    row.append(grant_start_date)
-    row.append(grant_end_date)
-    row.append(grant_note)
-    row.extend([''] * 3)  # doc_id_type #doc_id_value #doc_id_role
-    return row
-
-
-def makeCopyrightRow(filename, copyrightStartYear, copyrightTerm):
-    determination_date = datetime.now().strftime("%Y-%m-%d")
-    start_date = copyrightStartYear + "-01-01"
-    end_date = getCopyrightEndDate(copyrightStartYear, copyrightTerm)
-    note = "Copyright term 120 years from date of creation."
-    return makeRow(
-        filename,
-        'copyright',
-        'copyrighted',
-        determination_date,
-        'us',
-        start_date,
-        end_date,
-        note,
-        'publish',
-        'allow',
-        '2019-01-01',
-        'open',
-        '')
-
-
-def makePolicyRow(filename, microfilm):
-    if microfilm in [
-        'Rockefeller Sanitary Commission microfilm',
-        'Bureau of Social Hygiene microfilm',
-            'LSRM microfilm']:
-        note = "Rockefeller family/boards collections are open for research with select materials restricted."
-    return makeRow(
-        filename,
-        'policy',
-        '',
-        '',
-        '',
-        '1974-01-01',
-        'open',
-        note,
-        'disseminate',
-        'allow',
-        '2019-01-01',
-        'open',
-        '')
-
-
-def createRightsCsv(objectsDirectory, metadataDirectory, officer):
-    filenames = os.listdir(objectsDirectory)
-    with open(os.path.join(metadataDirectory, 'rights.csv'), 'w', newline='') as spreadsheet:
-        column_headings = [
-            "file",
-            "basis",
-            "status",
-            "determination_date",
-            "jurisdiction",
-            "start_date",
-            "end_date",
-            "terms",
-            "citation",
-            "note",
-            "grant_act",
-            "grant_restriction",
-            "grant_start_date",
-            "grant_end_date",
-            "grant_note",
-            "doc_id_type",
-            "doc_id_value",
-            "doc_id_role"]
-        writer(spreadsheet).writerow(column_headings)
-        # for each file, write copyright and donor rows
-        for f in filenames:
-            if f not in ['access', 'service', '.DS_Store', 'Thumbs.db']:
-                filename = "objects/" + f
-                writer(spreadsheet).writerow(
-                    makeCopyrightRow(filename, findDate(f), "120"))
-                writer(spreadsheet).writerow(makePolicyRow(filename, officer))
-    print('Done!')
-
-
 for folder in list_of_folders:
-    package_transfer(args.sip_directory, path_to_reel, args.collection, args.reel, folder)
+    package_transfer(
+        args.sip_directory,
+        path_to_reel,
+        args.collection,
+        args.reel,
+        folder)
