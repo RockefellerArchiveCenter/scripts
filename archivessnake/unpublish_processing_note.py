76615321aff267f8a9bd4486588a3382a77c1d05
kemartin2015 <k.martin@rockarch.org>
2020-12-09 12:37:29 -0500

    Update unpublish_processing_note.py

--- archivessnake/unpublish_processing_note.py
+++ archivessnake/unpublish_processing_note.py
@@ -18,7 +18,9 @@ start_time = time.time()
 
 def unpublish_processinfo_resources():
     for object_type in ['resources']:
+        print(object_type)
         for object in getattr(aspace, object_type):
+            print(object)
             if (object.jsonmodel_type == 'resource' and object.id_0.startswith(("FA"))):
                 for note in object.notes:
                     if (note.type == "processinfo"):
@@ -30,29 +32,23 @@ def unpublish_processinfo_resources():
                             print("Resource {} updated".format(object.uri))
                         else:
                             print(updated.json())
-
 
 def unpublish_processinfo_ao():
-    for object_type in ['archival_objects']:
-        for object in getattr(aspace, object_type):
-            if (object.jsonmodel_type == 'resource' and object.id_0.startswith(("FA"))):
+    for object in repo.archival_objects:
+            if (object.jsonmodel_type == 'archival_object'):
                 for note in object.notes:
-                    #print(note)
                     if (note.type == "processinfo"):
-                        #print("hello")
                         object_json = object.json()
                         note_json = note.json()
-                        #print(note_json)
                         note_json["publish"] = False
                         updated = aspace.client.post(object.uri, json=object_json)
                         if updated.status_code == 200:
-                            print("Resource {} updated".format(object.uri))
+                            print("Archival object {} updated".format(object.uri))
                         else:
                             print(updated.json())
 
-unpublish_processinfo_resources()
+#unpublish_processinfo_resources()
 
-#unpublish_processinfo_ao()
-
+unpublish_processinfo_ao()
 elapsed_time = time.time() - start_time
 print("Time Elapsed: " + time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
