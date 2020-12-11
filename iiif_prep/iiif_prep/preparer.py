10f5339c7c8ccdcd3bf6c58678071fe5717188e9
bonniegee <bgordon@rockarch.org>
2020-12-11 11:15:46 -0500

    variables to attributes

--- iiif_prep/iiif_prep/preparer.py
+++ iiif_prep/iiif_prep/preparer.py
@@ -38,35 +38,46 @@ class Preparer():
                     source_directory,
                     d))]
 
-    def determine_structure(self, officer_path, officer):
+    def determine_structure(self, officer):
         """docstring for find_mezzanine_directory"""
         structure = None
-        if "TIFFs" in listdir(officer_path):
+        if "TIFFs" in listdir(self.officer_path):
             structure = "CURRENT"
-        elif "Roll" in str(listdir(officer_path)):
+        elif "Roll" in str(listdir(self.officer_path)):
             structure = "MICROFILM"
-        elif officer in listdir(officer_path)[0].split('_', 1)[0]:
+        elif officer in listdir(self.officer_path)[0].split('_', 1)[0]:
             structure = "LEGACY"
         else:
             raise Exception(
                 "{} does not conform to standard structure".format(officer))
         return structure
 
-    def get_list_of_diaries(self, officer_path, structure):
+    def get_list_of_diaries(self):
         """Returns name that matches PDF title, and can be used in filepath to mezzanine directory"""
         diaries_list = []
-        if structure == "CURRENT":
-            mezzanine_directory = join(officer_path, "TIFFs", "Master-Edited")
+        if self.structure == "CURRENT":
+            mezzanine_directory = join(
+                self.officer_path, "TIFFs", "Master-Edited")
             diaries_list = [
                 d for d in listdir(mezzanine_directory) if isdir(
                     join(
                         mezzanine_directory,
                         d))]
-        elif structure == "LEGACY":
-            diaries_list = [d for d in listdir(officer_path) if isdir(join(officer_path, d))]
-        elif structure == "MICROFILM":
-            tiff_directory = [d for d in listdir(officer_path) if "Tiff" in d]
-            diaries_list = [d for d in listdir(join(officer_path, tiff_directory[0])) if isdir(join(officer_path, tiff_directory[0], d))]
+        elif self.structure == "LEGACY":
+            diaries_list = [d for d in listdir(
+                self.officer_path) if isdir(join(self.officer_path, d))]
+        elif self.structure == "MICROFILM":
+            tiff_directory = [d for d in listdir(
+                self.officer_path) if "Tiff" in d]
+            diaries_list = [
+                d for d in listdir(
+                    join(
+                        self.officer_path,
+                        tiff_directory[0])) if isdir(
+                    join(
+                        self.officer_path,
+                        tiff_directory[0],
+                        d))]
         return diaries_list
         
     def get_refids(self, officer, diaries_list):
