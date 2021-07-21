1bca4fd9b28be94917b5d5471c022f44dfb54624
Hillel Arnold <helrond@hotmail.com>
2021-07-21 16:16:56 -0400

    removing obsolete scripts

--- bagit/batch_bag.bat
+++
@@ -1,3 +0,0 @@
-c:\
-cd c:\bagit-4.4\bin
-bag create Y:\HudsonMicroImaging\RF_Diaries\Tennant_01_bag Y:\HudsonMicroImaging\RF_Diaries\Tennant_01_bag_old\data\* && bag create Y:\HudsonMicroImaging\RF_Diaries\Tennant_02_bag Y:\HudsonMicroImaging\RF_Diaries\Tennant_02_bag_old\data\* 
