8f5a11085563af632d25b3a6b11a8def6062ace9
Hillel Arnold <helrond@hotmail.com>
2022-10-03 10:58:31 -0400

    add key argument

--- utils/split_json.py
+++ utils/split_json.py
@@ -14,19 +14,21 @@ import os
 
 
 class JSONSplitter:
-    def __init__(self, filepath):
+    def __init__(self, filepath, key):
         self.filepath = filepath
+        self.key = key
         with open(filepath, 'r') as jf:
             self.data = json.load(jf)
 
     def split(self):
         for obj in self.data:
-            with open(os.path.join(os.path.split(self.filepath)[0], "{}.json".format(obj['id'])), 'w+') as df:
+            with open(os.path.join(os.path.split(self.filepath)[0], f"{obj[self.key]}.json", 'w+') as df:
                 json.dump(obj, df, indent=2)
         os.remove(self.filepath)
 
 parser = argparse.ArgumentParser(description='Splits JSON arrays into individual files.')
 parser.add_argument('filepath', help='File path of JSON file to split into separate files.')
+parser.add_argument('key', help='Key in the individual JSON objects to use as a filename.')
 args = parser.parse_args()
 
-JSONSplitter(args.filepath).split()
+JSONSplitter(args.filepath, args.key).split()
