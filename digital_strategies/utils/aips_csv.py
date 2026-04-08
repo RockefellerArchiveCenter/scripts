54ce2a0a5bba2ae4822d0a6e644cfedeba5e619b
ioduok <imaoduok@gmail.com>
2026-04-08 12:00:09 -0400

    add aips_csv script

---
+++ digital_strategies/utils/aips_csv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+#This script generates a csv file of AIPs in a specific Archivematica Storage Service location.
+
+import csv
+from amclient import AMClient
+
+SS_API_KEY = ""
+SS_USER_NAME = ""
+SS_URL = "http://archivematica-storage-service.dev.rockarch.org:8000/"
+
+
+def main():
+    archivematica_client = AMClient(
+            ss_api_key=SS_API_KEY,
+            ss_user_name=SS_USER_NAME,
+            ss_url=SS_URL)
+    with open('aips.csv', 'w', newline='') as csvfile:
+        fieldnames = ['uuid', 'current_location', 'origin_pipeline', 'status']
+        
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
+        writer.writeheader()
+
+        for aip in archivematica_client.aips():
+            writer.writerow(aip)
+
+
+if __name__ == '__main__':
+    main()
